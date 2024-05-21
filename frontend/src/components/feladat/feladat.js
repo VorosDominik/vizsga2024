b4f99fb8594381c9c962baa09bec438a113a4403
VorosDominik <medoxidhun@gmai.com>
2024-05-21 17:59:05 +0200

    feladat 2

--- frontend/src/components/feladat/feladat.js
+++ frontend/src/components/feladat/feladat.js
@@ -1,28 +1,25 @@
-import { useEffect, useState } from 'react';
-import './feladat.css';
-import { Card ,Button, Form } from 'react-bootstrap';
-import { getValue } from '@testing-library/user-event/dist/utils';
+import { useEffect, useState } from "react";
+import "./feladat.css";
+import { Card, Button, Form } from "react-bootstrap";
+import { getValue } from "@testing-library/user-event/dist/utils";
 
 function Feladat(props) {
-    const [feladat, setfeladat] = useState(props.feladat);
-    const [valasz, setvalasz] = useState();
- 
-  
-     
-  
+  const [feladat, setfeladat] = useState(props.feladat);
+  const [valasz, setvalasz] = useState();
+
+  const valaszBealitas = () => {
+    setvalasz();
+  };
+
   return (
     <div className="Feladat">
-   
-    <Card style={{ width: '18rem' }}>
-  
-      <Card.Body>
-      <div className='paros'>
-      <div className='Angol'>{feladat.Angol}</div>
-   
-      <div className='magyar'>   <input type="text"  /></div></div>
-
-      </Card.Body>
-    </Card>
+      <div className="paros">
+        <div className="Angol">{feladat.Angol}</div>
+        <div className="magyar">
+          <input type="text"  onChange={(e) => setvalasz(e.target.value)} />
+        </div>
+        <div></div>
+      </div>
     </div>
   );
 }
